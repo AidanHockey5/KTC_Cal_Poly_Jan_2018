01e2fce5bf7abd8771debc4082c0257f70506d76
Aidan Lawrence <aidan@aidanlawrence.com>
2018-01-11 09:36:22 -0800

    init

---
+++ Assets/Bomb.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bomb : MonoBehaviour
+{
+    public float boomTime = 5f;
+    public float power = 10f;
+    public float radius = 10f;
+    public GameObject expPtc;
+    public AudioClip expSound;
+
+    // Use this for initialization
+    void Start ()
+    {
+        Invoke("Explosion", boomTime);
+	}
+	
+	void Explosion ()
+    {
+        Vector3 expPos = transform.position;
+        Collider[] cols = Physics.OverlapSphere(expPos, radius);
+        foreach(Collider hit in cols)
+        {
+            Rigidbody rb = hit.GetComponent<Rigidbody>();
+            if (rb != null)
+                rb.AddExplosionForce(power, expPos, radius, 3.0f);
+        }
+        GameObject ptc = Instantiate(expPtc, expPos, Quaternion.identity);
+        AudioSource.PlayClipAtPoint(expSound, expPos);
+        Destroy(ptc, 10f);
+        Destroy(gameObject);
+	}
+}
---
+++ Assets/ColorChange.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ColorChange : MonoBehaviour
+{
+    Renderer r;
+	// Use this for initialization
+	void Start ()
+    {
+        r = GetComponent<Renderer>();
+        //r.material.color = Color.red;
+        StartCoroutine(Swap());
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+		
+	}
+
+    IEnumerator Swap()
+    {
+        while(true)
+        {
+            byte red = (byte)Random.Range(0, 255);
+            byte green = (byte)Random.Range(0, 255);
+            byte blue = (byte)Random.Range(0, 255);
+            r.material.color = new Color32(red, green, blue, 255);
+            yield return new WaitForSeconds(0.1f);
+        }
+    }
+
+
+}
