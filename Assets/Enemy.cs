fd7e397146c9cb44499f21b664b087fa8e37c6e3
Aidan Lawrence <aidan@aidanlawrence.com>
2018-01-12 15:08:04 -0800

    Last day

--- Assets/Enemy.cs
+++ Assets/Enemy.cs
@@ -20,39 +20,39 @@ public class Enemy : MonoBehaviour
     public float agroRange = 10f;
     float timer = 0f;
     Vector3 targetWaypoint;
-	// Use this for initialization
-	void Start ()
+    // Use this for initialization
+    void Start()
     {
         nav = GetComponent<NavMeshAgent>();
         waypoints = GameObject.FindGameObjectsWithTag("waypoint");
         player = GameObject.FindGameObjectWithTag("Player");
         targetWaypoint = PickNewWaypoint();
         //nav.SetDestination(targetWaypoint);
-	}
-	
+    }
+
     Vector3 PickNewWaypoint()
     {
         return waypoints[Random.Range(0, waypoints.Length)].transform.position;
     }
 
-	// Update is called once per frame
-	void Update ()
+    // Update is called once per frame
+    void Update()
     {
-		if(waypoints.Length == 0)
+        if (waypoints.Length == 0)
         {
             Debug.LogError("No waypoints found!");
             return;
         }
-        if(state == State.PATROL)
+        if (state == State.PATROL)
         {
             nav.destination = targetWaypoint;
             if (Vector3.Distance(transform.position, targetWaypoint) <= 1f)
                 state = State.WAIT;
         }
-        if(state == State.WAIT)
+        if (state == State.WAIT)
         {
             timer += Time.deltaTime;
-            if(timer >= timeToWait)
+            if (timer >= timeToWait)
             {
                 timer = 0f;
                 state = State.PATROL;
@@ -61,12 +61,12 @@ public class Enemy : MonoBehaviour
         }
         if (Vector3.Distance(player.transform.position, transform.position) < agroRange)
             state = State.ATTACK; //Distance check to player
-        else if(state == State.ATTACK) //Did the player get away?
+        else if (state == State.ATTACK) //Did the player get away?
         {
             state = State.PATROL;
             targetWaypoint = PickNewWaypoint();
         }
         if (state == State.ATTACK) //Attack the player 
             nav.destination = player.transform.position;
-	}
+    }
 }
---
+++ Assets/UIController.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIController : MonoBehaviour
+{
+    public Text winText;
+	// Use this for initialization
+	void Start ()
+    {
+        winText.enabled = false;
+	}
+	
+    public void ShowWinText()
+    {
+        winText.enabled = true;
+    }
+}
